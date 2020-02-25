45d458d9935449d6bbfd2a51dcdff4a099a068ea
Jonlemos <lemos.anjos@gmail.com>
2020-02-25 20:58:34 -0300

    Ajusto de slide show, alinhamentos

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ $('.carousel').slick({
     infinite: false,
     speed: 1000,
     fade:false,
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 4,
     responsive:[
     {
         breakpoint: 1024,
         settings:{
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             infinite: true,
             dots: true,
         }
@@ -18,8 +18,8 @@ $('.carousel').slick({
     {
         breakpoint: 600,
         settings:{
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 1,
+            slidesToScroll: 1,
         }
     },
     {
